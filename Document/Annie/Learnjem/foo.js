7353cc0013bdcbc5fe818d6ecd3c0eaba7578718
Maggie <maggie@gmail.com>
2021-02-18 15:49:45 +0530

    This is first commit

--- Document/Annie/Learnjem/foo.js
+++ Document/Annie/Learnjem/foo.js
@@ -1,6 +1,6 @@
 I am Anniie
-var doc="Annie master";
-var doc="Annie Email";
+var doc="Annie master init";
+var doc="Annie Email git";
 var doc="Annie Hii";
 var doc="Annie Hello";
 var doc="Annie";
